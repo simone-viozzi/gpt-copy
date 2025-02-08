f6f40d2640d771487fbcd69a467796043d5682da
simone-viozzi <simoneviozzi97@gmail.com>
2025-02-08 16:32:45 +0100

    add shell.nix for development environment setup with Python packages and dependencies

---
+++ shell.nix
@@ -0,0 +1,45 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+let
+  pythonPackages = pkgs.python311Packages; # Change to Python 3.10
+in pkgs.mkShell rec {
+  name = "concatenate-files";
+  venvDir = "./.venv";
+
+  buildInputs = with pkgs; [
+    gcc # Required for crates needing C compilers
+    pkg-config # Helps locate libraries like OpenSSL
+    openssl # OpenSSL library for crates like openssl-sys
+    git
+    git-crypt
+    stdenv.cc.cc.lib
+    stdenv.cc.cc # jupyter lab needs
+    pythonPackages.python
+    pythonPackages.pyzmq # Adding pyzmq explicitly
+    pythonPackages.venvShellHook
+    pythonPackages.pip
+    pythonPackages.ruff
+    pythonPackages.click
+    pythonPackages.pathspec
+    pythonPackages.tqdm
+    pre-commit
+  ];
+
+  postVenvCreation = ''
+    unset SOURCE_DATE_EPOCH
+  '';
+
+  pre-commit = pkgs.pre-commit;
+
+  postShellHook = ''
+    # allow pip to install wheels
+    unset SOURCE_DATE_EPOCH
+
+    pip install --upgrade wheel setuptools
+    export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+
+    echo "Environment setup complete."
+  '';
+}
